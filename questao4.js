b8375b0aa57c0f4cb452154e2aa5186d10a0efb6
Gabriel Pires <gabrielpires@poli.ufrj.br>
2024-10-26 14:09:34 -0300

    fix: fix sort names

--- questao4.js
+++ questao4.js
@@ -28,7 +28,7 @@ class Aluno {
     }
 }
 
-class Turma {
+export class Turma {
     constructor(alunos) {
         this.alunos = new Map(alunos.map((aluno) => [aluno.matricula, aluno]));
     }
@@ -40,7 +40,7 @@ class Turma {
     }
 
     addAluno() {
-        console.log("Adicionar Aluno");
+        console.log("\nAdicionar Aluno");
         const aluno = new Aluno();
         if (this.alunos.has(aluno.matricula)) {
             throw new Error("Aluno ja existe");
@@ -49,16 +49,26 @@ class Turma {
         return aluno;
     }
 
-    removeAluno(matricula) {
+    removeAluno() {
+        console.log("\nRemove Aluno");
+        const matricula = prompt("Matricula: ");
         this.#valida_matricula(matricula);
         this.alunos.delete(matricula);
     }
 
-    setNota(matricula) {
-        console.log(`Provas do ${aluno.nome}`);
-        const prova = parseInt(prompt(`Prova (1 ou 2): `));
-        this.#valida_matricula(matricula);
+    setNota() {
+        const matricula = prompt("Matricula Para Nota: ");
         const aluno = this.alunos.get(matricula);
+        this.#valida_matricula(matricula);
+        console.log(`Provas do ${aluno.nome}`);
+        while (true) {
+            const prova = parseInt(prompt(`Prova (1 ou 2): `));
+            if (prova !== 1 && prova !== 2) {
+                console.log("Prova Invalida");
+                continue;
+            }
+            break;
+        }
         aluno.notaProva = prova;
     }
 
@@ -70,14 +80,17 @@ class Turma {
     }
 
     notasTurma() {
-        console.log("---------------------------------");
+        const alunos = new Map([...this.alunos.entries()].sort(([, alunoA], [, alunoB]) => alunoA.nome.localeCompare(alunoB.nome)));
+
+        console.log("\n---------------------------------");
         console.log("Matricula Nome        P1  P2  NF ");
         console.log("---------------------------------");
-        for (const [matricula, aluno] of this.alunos) {
+
+        alunos.forEach((aluno) => {
             console.log(
-                `${matricula}  ${aluno.nome} ${this.#parseNota(aluno.p1)} ${this.#parseNota(aluno.p2)} ${this.#parseNota(aluno.notaFinal)}`
+                `${aluno.matricula}  ${aluno.nome} ${this.#parseNota(aluno.p1)} ${this.#parseNota(aluno.p2)} ${this.#parseNota(aluno.notaFinal)}`
             );
-        }
+        })
         console.log("-----------------------------");
     }
 }
@@ -93,14 +106,14 @@ class Turma {
 //const aluno3 = new Aluno();
 //
 //const turma = new Turma([aluno1, aluno2, aluno3]);
-//const aluno4 = turma.addAluno();
+//turma.addAluno();
 //
-//turma.removeAluno(aluno2.matricula);
+//turma.removeAluno();
 //
-//turma.setNota(aluno1.matricula);
-//turma.setNota(aluno1.matricula);
-//turma.setNota(aluno3.matricula);
-//turma.setNota(aluno4.matricula);
+//turma.setNota();
+//turma.setNota();
+//turma.setNota();
+//turma.setNota();
 //
 //turma.notasTurma();
 //console.log("Fim da Questão 4");
